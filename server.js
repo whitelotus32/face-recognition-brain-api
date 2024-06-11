61cd868689ddb1d3ad0a842c277770f6fc65a265
Nicholas Dixon <NickDixon32@proton.me>
2024-06-10 22:53:59 -0500

    Built the routes for profile and images.

--- server.js
+++ server.js
@@ -51,6 +51,34 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length-1])
 })
 
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(users => {
+        if (users.id === id) {
+            found = true;
+            return res.json(user);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(users => {
+        if (users.id === id) {
+            found = true;
+            users.entries++
+            return res.json(users.entries);
+        } 
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
 })
