7be2e47747821cad9226beb149d363042df6655b
Nicholas Dixon <naseer2152@gmail.com>
2024-07-02 16:36:25 -0500

    Updated the profile/id endpoint.

--- server.js
+++ server.js
@@ -14,8 +14,6 @@ const db = knex({
     },
 });
 
-db.select('*').from('users');
-
 const app = express();
 
 const database = {
@@ -57,31 +55,30 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    database.users.push({
-        id: '125',
-        name: 'name',
-        email: 'email',
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1])
+    db('users')
+        .returning('*')
+        .insert({
+            email: email,
+            name: name,
+            joined: new Date()
+        })
+        .then(user => {
+            res.json(user[0]);
+        })
+        .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(users => {
-        if (users.id === id) {
-            found = true;
-            return res.json(user);
-        } 
+    db.select('*').from('users').where({id})
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('not found')
+        }
     })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
