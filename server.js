fad21b25da59a4794e8fe2239b920a2b23c396da
Nicholas Dixon <naseer2152@gmail.com>
2024-07-02 19:50:56 -0500

    Updated the image endpoint.

--- server.js
+++ server.js
@@ -83,17 +83,13 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(users => {
-        if (users.id === id) {
-            found = true;
-            users.entries++
-            return res.json(users.entries);
-        } 
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
     })
-    if (!found) {
-        res.status(400).json('not found');
-    }
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 
