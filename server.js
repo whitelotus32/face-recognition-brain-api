fa2ac62cdcc04ac9004cea89e2c71f8b6b7a5316
Nicholas Dixon <naseer2152@gmail.com>
2024-07-03 19:19:59 -0500

    Cleaned up the register endpoint.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
-const knex = require('knex')
+const knex = require('knex');
+
+const register = require('./controllers/register');
 
 const db = knex({
     client: 'pg',
@@ -42,33 +44,7 @@ app.post('/signin', (req, res) => {
     .catch(err => res.status(400).json('wrong credentials'))
 })
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-                .returning('*')
-                .insert({
-                    email: loginEmail[0].email,
-                    name: name,
-                    joined: new Date()
-                })
-                .then(user => {
-                    res.json(user[0]);
-                })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'))
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
