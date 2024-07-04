6c8027ac43992e1657008fa525ed647fd70c9d69
Nicholas Dixon <naseer2152@gmail.com>
2024-07-03 20:23:43 -0500

    Cleaned up the image endpoint.

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 const knex = require('knex');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
 const db = knex({
     client: 'pg',
@@ -23,26 +25,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 
-app.get('/', (req, res)=> {
-    res.send(database.users);
-})
-
+app.get('/', (req, res) => { res.send(database.users)})
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
-
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
-
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries => {
-        res.json(entries[0].entries);
-    })
-    .catch(err => res.status(400).json('error getting user'))
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
