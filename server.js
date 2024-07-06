7116a01c417fb9c43aaea089ccc30d3cca63fd25
Nicholas Dixon <naseer2152@gmail.com>
2024-07-06 15:54:02 -0500

    Cleaned up code for deployment.

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors())
 
 app.get('/', (req, res) => { res.send(database.users)})
-app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
